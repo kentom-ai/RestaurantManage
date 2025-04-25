b6bf9521861e480daeecfdb16f5b42b5724b832a
kentom-ai <kbt2022-6672-99876@bicol-u.edu.ph>
2025-04-25 20:51:30 +0800

    Add files via upload

---
+++ RestManageSystem/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace RestManageSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            Customers = new Button();
+            Inventory = new Button();
+            groupBox1 = new GroupBox();
+            button7 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(342, 33);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(642, 383);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(27, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(257, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Restaurant Management System";
+            label1.Click += label1_Click_1;
+            // 
+            // Customers
+            // 
+            Customers.AccessibleDescription = "";
+            Customers.AccessibleName = "";
+            Customers.Location = new Point(27, 78);
+            Customers.Name = "Customers";
+            Customers.Size = new Size(257, 32);
+            Customers.TabIndex = 1;
+            Customers.Text = "Customers";
+            Customers.UseVisualStyleBackColor = true;
+            Customers.Click += button1_Click;
+            // 
+            // Inventory
+            // 
+            Inventory.Location = new Point(27, 139);
+            Inventory.Name = "Inventory";
+            Inventory.Size = new Size(257, 32);
+            Inventory.TabIndex = 2;
+            Inventory.Text = "Inventory";
+            Inventory.UseVisualStyleBackColor = true;
+            Inventory.Click += button2_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(Inventory);
+            groupBox1.Controls.Add(Customers);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(21, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(303, 500);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(27, 432);
+            button7.Name = "button7";
+            button7.Size = new Size(257, 32);
+            button7.TabIndex = 7;
+            button7.Text = "Suppliers";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(27, 373);
+            button4.Name = "button4";
+            button4.Size = new Size(257, 32);
+            button4.TabIndex = 6;
+            button4.Text = "Staffs";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(27, 260);
+            button5.Name = "button5";
+            button5.Size = new Size(257, 32);
+            button5.TabIndex = 5;
+            button5.Text = "OrdersDetails";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(27, 317);
+            button6.Name = "button6";
+            button6.Size = new Size(257, 32);
+            button6.TabIndex = 4;
+            button6.Text = "Order";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(27, 198);
+            button3.Name = "button3";
+            button3.Size = new Size(257, 32);
+            button3.TabIndex = 3;
+            button3.Text = "Menu";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(342, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 21);
+            label2.TabIndex = 2;
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1011, 549);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button Customers;
+        private Button Inventory;
+        private GroupBox groupBox1;
+        private Button button7;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button3;
+        private Label label2;
+    }
+}
---
+++ RestManageSystem/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RestManageSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void label1_Click_1(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void groupBox1_Enter(object sender, EventArgs e) { }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+
+        // button1 = Customers
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Customer> customers = new List<Customer>(); // Empty list
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = customers;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button2 = Inventory
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Inventory> inventory = new List<Inventory>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = inventory;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button3 = Menu
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Menu> menu = new List<Menu>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = menu;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button6 = Orders
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<Order> orders = new List<Order>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = orders;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button5 = Order Details
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = orderDetails;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button4 = Staffs
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<Staff> staffs = new List<Staff>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = staffs;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // button7 = Suppliers
+        private void button7_Click(object sender, EventArgs e)
+        {
+            List<Supplier> suppliers = new List<Supplier>();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = suppliers;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+
+    // Entity Classes
+
+    public class Customer
+    {
+        public int customer_id { get; set; }
+        public string lname { get; set; }
+        public string fname { get; set; }
+        public string phone_number { get; set; }
+        public string address { get; set; }
+    }
+
+    public class Staff
+    {
+        public int staff_id { get; set; }
+        public string lname { get; set; }
+        public string fname { get; set; }
+        public string role { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+    }
+
+    public class Menu
+    {
+        public int menu_id { get; set; }
+        public string item_name { get; set; }
+        public string category { get; set; }
+        public string price { get; set; }
+    }
+
+    public class Inventory
+    {
+        public int inventory_id { get; set; }
+        public string item_name { get; set; }
+        public string quantity_in_stock { get; set; }
+        public string reorder_level { get; set; }
+        public int supplier_id { get; set; }
+        public int staff_id { get; set; }
+    }
+
+    public class Order
+    {
+        public int order_id { get; set; }
+        public int customer_id { get; set; }
+        public int staff_id { get; set; }
+        public string order_date { get; set; }
+        public float total_amount { get; set; }
+    }
+
+    public class OrderDetail
+    {
+        public int order_detail_id { get; set; }
+        public int order_id { get; set; }
+        public int menu_id { get; set; }
+        public string quantity { get; set; }
+        public float subtotal { get; set; }
+    }
+
+    public class Supplier
+    {
+        public int supplier_id { get; set; }
+        public string name { get; set; }
+        public string contact_person { get; set; }
+        public string phone_number { get; set; }
+        public string address { get; set; }
+    }
+}
